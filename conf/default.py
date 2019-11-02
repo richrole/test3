0112c90b5327e121951fde39bb9abb0c4d22188a
Rich <1129163932@qq.com>
2019-11-02 11:40:14 +0800

    cccc

--- conf/default.py
+++ conf/default.py
@@ -22,12 +22,12 @@ from django.conf.global_settings import *  # noqa
 # 应用基本信息配置 (请按照说明修改)
 # ==============================================================================
 # 在蓝鲸智云开发者中心 -> 点击应用ID -> 基本信息 中获取 APP_ID 和 APP_TOKEN 的值
-APP_ID = 'bk_qi'
-APP_TOKEN = 'f5330765-a796-4f0e-af5b-8dfad4d9c089'
+APP_ID = 'rich'
+APP_TOKEN = 'f714f891-068d-4596-a981-b282a2df1ee1'
 # 蓝鲸智云开发者中心的域名，形如：http://paas.bking.com
 BK_PAAS_HOST = 'https://paas.dev2.com'
 
-# 请求官方 API 默认版本号，可选值为："v2" 或 ""；其中，"v2"表示规范化API，""表示未规范化API
+# 请求官方 API 默py认版本号，可选值为："v2" 或 ""；其中，"v2"表示规范化API，""表示未规范化API
 DEFAULT_BK_API_VER = 'v2'
 
 # 是否启用celery任务
--- conf/settings_development.py
+++ conf/settings_development.py
@@ -12,9 +12,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': 'root',                        # 你的数据库user
-        'PASSWORD': 'root',                        # 你的数据库password
-        'HOST': '127.0.0.1',                   # 开发的时候，使用localhost
+        'USER': 'examer',                        # 你的数据库user
+        'PASSWORD': 'mypwd',                        # 你的数据库password
+        'HOST': '192.168.165.187',                   # 开发的时候，使用localhost
         'PORT': '3306',                        # 默认3306
     },
 }
